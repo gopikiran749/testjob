3218b89681cee2bf588d937d83d7a956a39b7a6e
gopikiran749 <54103533+gopikiran749@users.noreply.github.com>
2021-12-21 14:02:22 +0530

    Update awscli.sh

--- awscli.sh
+++ awscli.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
 yum install unzip -y
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 aws --version
+git --version
+java -version
+cd /etc/yum.repos.d
+ls -la
+
+for X in {1..20}
+do
+echo "the value of x is $X now....."
+sleep 3
+done
