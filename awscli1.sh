51c2cc02c72bb4004da3854b88e3214e6a7aee7c
gopikiran749 <54103533+gopikiran749@users.noreply.github.com>
2022-01-11 10:16:22 +0530

    Update awscli1.sh

--- awscli1.sh
+++ awscli1.sh
@@ -2,3 +2,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 aws --version
+ 199  aws ec2 describe-volumes
+  200  aws ec2 describe-instances
+  201  aws ec2 describe-vpcs
+  202  aws ec2 describe-keypairs
+  203  aws ec2 describe-key-pairs
+
